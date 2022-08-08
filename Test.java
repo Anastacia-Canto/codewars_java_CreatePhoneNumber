58dd8ce73b0480a331d05d59ea0c005115d72231
Anastácia Canto <91332251+Anastacia-Canto@users.noreply.github.com>
2022-08-08 21:48:13 +0100

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Kata.createPhoneNumber(new int[] {3, 5, 1, 4, 5, 6, 7, 8, 9, 0}));
+		System.out.println(Kata.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+	}
+}
